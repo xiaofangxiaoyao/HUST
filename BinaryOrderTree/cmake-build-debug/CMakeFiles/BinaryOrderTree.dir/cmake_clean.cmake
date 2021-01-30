9afbd425cdc61c6951a7f106df2643714c731816
xiaofangxiaoyao <2428300273@qq.com>
2021-01-30 18:53:05 +0800

    new file:   BinaryOrderTree/.idea/.gitignore         new file:   BinaryOrderTree/.idea/BinaryOrderTree.iml         new file:   BinaryOrderTree/.idea/misc.xml         new file:   BinaryOrderTree/.idea/modules.xml         new file:   BinaryOrderTree/.idea/vcs.xml         new file:   BinaryOrderTree/CMakeLists.txt         new file:   BinaryOrderTree/cmake-build-debug/BinaryOrderTree.cbp         new file:   BinaryOrderTree/cmake-build-debug/CMakeCache.txt         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/DependInfo.cmake         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/build.make         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/cmake_clean.cmake         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/depend.make         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/flags.make         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/link.txt         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/linklibs.rsp         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/objects1.rsp         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/progress.make         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/Makefile2         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/progress.marks         new file:   BinaryOrderTree/cmake-build-debug/Makefile         new file:   BinaryOrderTree/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   BinaryOrderTree/cmake-build-debug/cmake_install.cmake         new file:   BinaryOrderTree/main.c

---
+++ BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BinaryOrderTree.exe"
+  "BinaryOrderTree.exe.manifest"
+  "BinaryOrderTree.pdb"
+  "CMakeFiles/BinaryOrderTree.dir/main.c.obj"
+  "libBinaryOrderTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BinaryOrderTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
