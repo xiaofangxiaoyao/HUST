d7e17a355968b63dc83cfcae33869d59b9446a29
xiaofangxiaoyao <2428300273@qq.com>
2021-01-30 16:25:07 +0800

    new file:   CheckIP/.idea/.gitignore         new file:   CheckIP/.idea/CheckIP.iml         new file:   CheckIP/.idea/misc.xml         new file:   CheckIP/.idea/modules.xml         new file:   CheckIP/.idea/vcs.xml         new file:   CheckIP/CMakeLists.txt         new file:   CheckIP/cmake-build-debug/CMakeCache.txt         new file:   CheckIP/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   CheckIP/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   CheckIP/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   CheckIP/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   CheckIP/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   CheckIP/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   CheckIP/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   CheckIP/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/DependInfo.cmake         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/build.make         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/cmake_clean.cmake         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/depend.make         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/flags.make         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/link.txt         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/linklibs.rsp         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/objects1.rsp         new file:   CheckIP/cmake-build-debug/CMakeFiles/CheckIP.dir/progress.make         new file:   CheckIP/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   CheckIP/cmake-build-debug/CMakeFiles/Makefile2         new file:   CheckIP/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   CheckIP/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   CheckIP/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   CheckIP/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   CheckIP/cmake-build-debug/CMakeFiles/progress.marks         new file:   CheckIP/cmake-build-debug/CheckIP.cbp         new file:   CheckIP/cmake-build-debug/Makefile         new file:   CheckIP/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   CheckIP/cmake-build-debug/cmake_install.cmake         new file:   CheckIP/main.c         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/C.includecache         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/depend.internal         modified:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/depend.make         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/main.c.obj         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/objects.a         new file:   SumChar/cmake-build-debug/SumChar.exe         modified:   SumChar/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   SumChar/data_in.txt         new file:   SumChar/data_out.txt         modified:   SumChar/main.c

--- SumChar/main.c
+++ SumChar/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include "string.h"
+void SumCharFile(void);
+void SumChar(void);
+
 
 int main() {
-    printf("Hello, World!\n");
+//    SumCharFile();
+    SumChar();
     return 0;
+}
+
+void SumCharFile(void){
+    FILE *fp_in;
+    FILE *fp_out;
+    char str[100];
+    fp_in=fopen("../data_in.txt","r");
+    fp_out=fopen("../data_out.txt","w+");
+    if (fp_in==NULL){
+        printf("can't read file");
+    }
+    if (fp_out==NULL){
+        printf("can't write file");
+    }
+    fscanf(fp_in,"%s",str);
+    int len=strlen(str);
+//    printf(str);
+    while (str[len-1]!='.'){
+        printf("%d ",len);
+        fscanf(fp_in,"%s",str);
+        len=strlen(str);
+    }
+    printf("%d\n",len-1);
+}
+void SumChar(void){
+    char str[100];
+    scanf("%s",str);
+    int len=strlen(str);
+//    printf(str);
+    while (str[len-1]!='.'){
+        printf("%d ",len);
+        scanf("%s",str);
+        len=strlen(str);
+    }
+    printf("%d\n",len-1);
 }
