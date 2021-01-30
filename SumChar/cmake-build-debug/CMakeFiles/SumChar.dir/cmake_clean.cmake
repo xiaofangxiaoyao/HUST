aa9059c837b33bfc1c7761836f65d42c958f87a7
xiaofangxiaoyao <2428300273@qq.com>
2021-01-30 15:24:27 +0800

    new file:   SumChar/.idea/.gitignore         new file:   SumChar/.idea/SumChar.iml         new file:   SumChar/.idea/misc.xml         new file:   SumChar/.idea/modules.xml         new file:   SumChar/.idea/vcs.xml         new file:   SumChar/CMakeLists.txt         new file:   SumChar/cmake-build-debug/CMakeCache.txt         new file:   SumChar/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   SumChar/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   SumChar/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   SumChar/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   SumChar/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   SumChar/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   SumChar/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   SumChar/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   SumChar/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   SumChar/cmake-build-debug/CMakeFiles/Makefile2         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/DependInfo.cmake         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/build.make         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/cmake_clean.cmake         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/depend.make         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/flags.make         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/link.txt         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/linklibs.rsp         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/objects1.rsp         new file:   SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/progress.make         new file:   SumChar/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   SumChar/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   SumChar/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   SumChar/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   SumChar/cmake-build-debug/CMakeFiles/progress.marks         new file:   SumChar/cmake-build-debug/Makefile         new file:   SumChar/cmake-build-debug/SumChar.cbp         new file:   SumChar/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   SumChar/cmake-build-debug/cmake_install.cmake         new file:   SumChar/main.c

---
+++ SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SumChar.dir/main.c.obj"
+  "SumChar.exe"
+  "SumChar.exe.manifest"
+  "SumChar.pdb"
+  "libSumChar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SumChar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SumChar/cmake-build-debug/CMakeFiles/SumChar.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SumChar.
+# This may be replaced when dependencies are built.
