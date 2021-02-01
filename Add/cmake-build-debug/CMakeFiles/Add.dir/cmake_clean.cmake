87bea90dee311d54f3e8ac915ded3d7d26abf3bb
xiaofangxiaoyao <2428300273@qq.com>
2021-02-01 18:30:22 +0800

    new file:   Add/.idea/.gitignore         new file:   Add/.idea/Add.iml         new file:   Add/.idea/misc.xml         new file:   Add/.idea/modules.xml         new file:   Add/.idea/vcs.xml         new file:   Add/CMakeLists.txt         new file:   Add/cmake-build-debug/Add.cbp         new file:   Add/cmake-build-debug/CMakeCache.txt         new file:   Add/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   Add/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   Add/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   Add/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   Add/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   Add/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/DependInfo.cmake         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/build.make         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/cmake_clean.cmake         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/depend.make         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/flags.make         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/link.txt         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/linklibs.rsp         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/objects1.rsp         new file:   Add/cmake-build-debug/CMakeFiles/Add.dir/progress.make         new file:   Add/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   Add/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   Add/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   Add/cmake-build-debug/CMakeFiles/Makefile2         new file:   Add/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   Add/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   Add/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   Add/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   Add/cmake-build-debug/CMakeFiles/progress.marks         new file:   Add/cmake-build-debug/Makefile         new file:   Add/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   Add/cmake-build-debug/cmake_install.cmake         new file:   Add/main.c         modified:   BigDate/.idea/BigDate.iml         new file:   BigDate/cmake-build-debug/BigDate.exe         new file:   BigDate/cmake-build-debug/CMakeFiles/BigDate.dir/C.includecache         new file:   BigDate/cmake-build-debug/CMakeFiles/BigDate.dir/depend.internal         modified:   BigDate/cmake-build-debug/CMakeFiles/BigDate.dir/depend.make         new file:   BigDate/cmake-build-debug/CMakeFiles/BigDate.dir/main.c.obj         new file:   BigDate/cmake-build-debug/CMakeFiles/BigDate.dir/objects.a         modified:   BigDate/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   BigDate/main.c

---
+++ Add/cmake-build-debug/CMakeFiles/Add.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Add.exe"
+  "Add.exe.manifest"
+  "Add.pdb"
+  "CMakeFiles/Add.dir/main.c.obj"
+  "libAdd.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- BigDate/main.c
+++ BigDate/main.c
@@ -1,6 +1,93 @@
 #include <stdio.h>
+#include "string.h"
+void BigData(void);
+void NewArray(char * a,int len);
+char* add(char*first ,int first_length,char *second,int second_length,int * maxlen);
+void clean(char*a,int  len);
+void printfarray(char* a,int len);
+void test();
 
 int main() {
-    printf("Hello, World!\n");
+    BigData();
+//    test();
     return 0;
 }
+
+void clean(char*a,int len){
+    for (int i = 0; i < len; ++i) {
+            a[i]=a[i]-'0';
+    }
+}
+void NewArray(char * a,int len){
+    char newarray[2000];
+    int i,j;
+    for ( i = 0,j=len-1; j>=0 ; ++i,--j) {
+        newarray[i]=a[j];
+    }
+    for (int k = 0; k < len; ++k) {//一定要注意，当char*指针传递时候遇到了‘\0'（可以写0）时候，对于后面所有操作全部视为不可见，所以要慎重
+        a[k]=newarray[k]+'0';
+    }
+}
+char* add(char*first ,int first_length,char *second,int second_length,int* maxlen){
+    clean(first,first_length);
+    clean(second,second_length);
+    NewArray(first,first_length);
+    NewArray(second,second_length);
+    for (int i = 0; i < *maxlen; ++i) {
+        if (i<first_length&&i<second_length){//同理
+            if (first[i]+second[i]-2*'0'>9){
+                first[i]=first[i]+second[i]-10-'0';
+                first[i+1]++;
+            } else{
+                first[i]=first[i]+second[i]-'0';
+            }
+        } else{
+            if (first[i]+second[i]-'0'>9){
+                first[i]=first[i]+second[i]-10;
+                first[i+1]++;
+            } else{
+                first[i]=first[i]+second[i];
+            }
+        }
+    }
+    if (first[*maxlen]!='\0'){
+        *maxlen++;
+    }
+    return first;
+}
+// print the array from the back to the front since without '\0'
+void printfarray(char* a,int len){
+    for (int i = len;i>=0; --i) {
+        if (a[i]!='\0'){
+            printf("%c",a[i]);
+        }
+    }
+    printf("\n");
+}
+
+
+
+void BigData(void){
+    char first[2000];
+    char second[2000];
+    char* total;
+    int first_length;
+    int second_length;
+    while (scanf("%s",first)!=EOF&&scanf("%s",second)!=EOF) {
+        first_length = strlen(first);
+        second_length = strlen(second);
+        int maxlen;
+        if (first_length > second_length){
+            maxlen=first_length;
+        } else{
+            maxlen=second_length;
+        }
+        total=add(first ,first_length,second,second_length,&maxlen);
+        printfarray(total,maxlen);
+        for (int i = 0; i < 1000; ++i) {
+            first[i]='\0';
+            second[i]='\0';
+        }
+    }
+}
+
