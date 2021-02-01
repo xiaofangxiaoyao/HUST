1ecba17f5389aee1a37c41adda34f9b4193340eb
xiaofangxiaoyao <2428300273@qq.com>
2021-02-01 13:42:59 +0800

    new file:   BinaryOrderTree/cmake-build-debug/BinaryOrderTree.exe         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/C.includecache         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/depend.internal         modified:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/depend.make         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/main.c.obj         new file:   BinaryOrderTree/cmake-build-debug/CMakeFiles/BinaryOrderTree.dir/objects.a         modified:   BinaryOrderTree/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   BinaryOrderTree/data_in.txt         modified:   BinaryOrderTree/main.c         new file:   StringConnect/.idea/.gitignore         new file:   StringConnect/.idea/StringConnect.iml         new file:   StringConnect/.idea/misc.xml         new file:   StringConnect/.idea/modules.xml         new file:   StringConnect/.idea/vcs.xml         new file:   StringConnect/CMakeLists.txt         new file:   StringConnect/cmake-build-debug/CMakeCache.txt         new file:   StringConnect/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   StringConnect/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   StringConnect/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   StringConnect/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   StringConnect/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   StringConnect/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   StringConnect/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   StringConnect/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   StringConnect/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   StringConnect/cmake-build-debug/CMakeFiles/Makefile2         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/DependInfo.cmake         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/build.make         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/cmake_clean.cmake         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/depend.make         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/flags.make         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/link.txt         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/linklibs.rsp         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/objects1.rsp         new file:   StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/progress.make         new file:   StringConnect/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   StringConnect/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   StringConnect/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   StringConnect/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   StringConnect/cmake-build-debug/CMakeFiles/progress.marks         new file:   StringConnect/cmake-build-debug/Makefile         new file:   StringConnect/cmake-build-debug/StringConnect.cbp         new file:   StringConnect/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   StringConnect/cmake-build-debug/cmake_install.cmake         new file:   StringConnect/main.c

--- BinaryOrderTree/main.c
+++ BinaryOrderTree/main.c
@@ -1,6 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void BinaryOrderTreeFile(void);
+void BinaryOrdeTree(void);
+typedef struct Node{
+    int value;
+    struct Node * left;
+    struct Node * right;
+}Node;
+Node* CreatNode(void);
+Node* InsertNode(struct Node* t,struct Node* f,int i);
 
 int main() {
-    printf("Hello, World!\n");
+//    BinaryOrderTreeFile();
+    BinaryOrdeTree();
     return 0;
+}
+/*creat a node */
+Node* CreatNode(void){
+    Node *t ;
+    t = (struct Node *)malloc(sizeof (struct Node));
+    t->value=0;
+    t->left=NULL;
+    t->right=NULL;
+    return t;
+}
+/*insert node
+ * t:point node now
+ * f:parents node
+ * i:value */
+Node* InsertNode(struct Node* t,struct Node* f,int i){
+    if (t==NULL){
+        t=CreatNode();
+        t->value=i;
+        if (f==NULL){
+            printf("-1\n");
+        } else{
+            printf("%d\n",f->value);
+        }
+    } else{
+        if (t->value>i){
+            t->left=InsertNode(t->left,t,i);
+        } else{
+            t->right=InsertNode(t->right,t,i);
+        }
+    }
+    return t;
+}
+
+
+void BinaryOrderTreeFile(void){
+    FILE * fp_in;
+    int Node_Number;
+    fp_in=fopen("../data_in.txt","r");
+    if (fp_in==NULL){
+        printf("can't read the file");
+    }
+    Node * t=NULL;
+    Node * f=NULL;
+    fscanf(fp_in,"%d",&Node_Number);
+    for (int i = 0; i < Node_Number; ++i) {
+        int value;
+        fscanf(fp_in,"%d",&value);
+        t=InsertNode(t,f,value);
+    }
+
+}
+void BinaryOrdeTree(void){
+    int Node_Number;
+    Node * t=NULL;
+    Node * f=NULL;
+    scanf("%d",&Node_Number);
+    for (int i = 0; i < Node_Number; ++i) {
+        int value;
+        scanf("%d",&value);
+        t=InsertNode(t,f,value);
+    }
 }
---
+++ StringConnect/cmake-build-debug/CMakeFiles/StringConnect.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for StringConnect.
+# This may be replaced when dependencies are built.
