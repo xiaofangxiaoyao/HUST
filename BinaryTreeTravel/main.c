3ec5f643d595281dffd2ecd880c0b54d5ae03711
xiaofangxiaoyao <2428300273@qq.com>
2021-02-02 11:43:27 +0800

    modified:   BinaryOrderTree/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   BinaryTreeTravel/cmake-build-debug/BinaryTreeTravel.exe         new file:   BinaryTreeTravel/cmake-build-debug/CMakeFiles/BinaryTreeTravel.dir/C.includecache         new file:   BinaryTreeTravel/cmake-build-debug/CMakeFiles/BinaryTreeTravel.dir/depend.internal         modified:   BinaryTreeTravel/cmake-build-debug/CMakeFiles/BinaryTreeTravel.dir/depend.make         new file:   BinaryTreeTravel/cmake-build-debug/CMakeFiles/BinaryTreeTravel.dir/main.c.obj         new file:   BinaryTreeTravel/cmake-build-debug/CMakeFiles/BinaryTreeTravel.dir/objects.a         modified:   BinaryTreeTravel/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   BinaryTreeTravel/data_in.txt         modified:   BinaryTreeTravel/main.c

--- BinaryTreeTravel/main.c
+++ BinaryTreeTravel/main.c
@@ -1,6 +1,104 @@
 #include <stdio.h>
+#include "string.h"
+#include "stdlib.h"
+typedef struct Node{ //in c if you don't use typedef ,everytime you use Node,you just need add typedef
+    char value;
+    struct Node *left;
+    struct Node* right;
+}Node;
+
+void BinaryTreeTravelFile(void);
+void BinaryTreeTravel(void);
+Node* CreateBinaryTree(char *front,char *middle,int number);//use front and middle to creat a tree,return tree
+void PostOrderTravel(Node *root);
+Node* CreatNode(void);//create a clean node
+int FindPlace(char *s,int number,char a);//return a place
+
 
 int main() {
-    printf("Hello, World!\n");
+//    BinaryTreeTravelFile();
+    BinaryTreeTravel();
     return 0;
+}
+Node* CreatNode(void){
+    struct Node * node =(struct Node *)malloc(sizeof (struct Node));
+    node->value='\0';
+    node->left=NULL;
+    node->right=NULL;
+    return node;
+}
+int FindPlace(char *s,int number,char a){
+    int i;
+    for (i = 0; i < number; ++i) {
+        if (s[i]==a){
+            break;
+        }
+    }
+    return i+1;
+}
+
+Node* CreateBinaryTree(char *front,char * middle,int number){
+    if (number==0){
+        return NULL;
+    } else{
+        struct Node * node =CreatNode();
+        node->value=front[0];
+//        printf("%c\n",front[0]);
+        int place;
+        place=FindPlace(middle,number,front[0]);
+        node->left=CreateBinaryTree(front+1,middle,place-1);
+        node->right=CreateBinaryTree(front+place,middle+place,number-place);
+        return node;
+    }
+}
+void PostOrderTravel(Node *root){
+    if (root->left==NULL&&root->right==NULL){
+        printf("%c",root->value);
+    } else{
+        if (root->left==NULL&&root->right!=NULL){
+            PostOrderTravel(root->right);
+            printf("%c",root->value);
+        } else{
+            if (root->right==NULL&&root->left!=NULL){
+                PostOrderTravel(root->left);
+                printf("%c",root->value);
+            } else{
+                PostOrderTravel(root->left);
+                PostOrderTravel(root->right);
+                printf("%c",root->value);
+            }
+        }
+
+    }
+
+}
+void BinaryTreeTravelFile(void){
+    char s1[27];
+    char s2[27];
+    FILE * fp_in;
+    int Node_Number;
+    fp_in=fopen("../data_in.txt","r");
+    if (fp_in==NULL){
+        printf("can't read the file");
+    }
+    int number;
+    Node *root;
+    while (fscanf(fp_in,"%s",s1)!=EOF&&fscanf(fp_in,"%s",s2)!=EOF){
+        number=strlen(s1);
+        root=CreateBinaryTree(s1,s2,number);
+        PostOrderTravel(root);
+        printf("\n");
+    }
+}
+void BinaryTreeTravel(void){
+    char s1[27];
+    char s2[27];
+    int number;
+    Node *root;
+    while (scanf("%s",s1)!=EOF&&scanf("%s",s2)!=EOF){
+        number=strlen(s1);
+        root=CreateBinaryTree(s1,s2,number);
+        PostOrderTravel(root);
+        printf("\n");
+    }
 }
