13e1ea24919696abb6977b497ca63fabcb43ac62
xiaofangxiaoyao <2428300273@qq.com>
2021-02-02 22:29:43 +0800

    new file:   MatrixMaX/cmake-build-debug/CMakeFiles/MatrixMaX.dir/C.includecache         new file:   MatrixMaX/cmake-build-debug/CMakeFiles/MatrixMaX.dir/depend.internal         modified:   MatrixMaX/cmake-build-debug/CMakeFiles/MatrixMaX.dir/depend.make         new file:   MatrixMaX/cmake-build-debug/CMakeFiles/MatrixMaX.dir/main.c.obj         new file:   MatrixMaX/cmake-build-debug/CMakeFiles/MatrixMaX.dir/objects.a         new file:   MatrixMaX/cmake-build-debug/MatrixMaX.exe         modified:   MatrixMaX/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   MatrixMaX/data.txt         modified:   MatrixMaX/main.c         new file:   MinAgeStaff/.idea/MinAgeStaff.iml         new file:   MinAgeStaff/.idea/misc.xml         new file:   MinAgeStaff/.idea/modules.xml         new file:   MinAgeStaff/.idea/vcs.xml         new file:   MinAgeStaff/.idea/workspace.xml         new file:   MinAgeStaff/CMakeLists.txt         new file:   MinAgeStaff/cmake-build-debug/CMakeCache.txt         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/Makefile2         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/DependInfo.cmake         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/build.make         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/cmake_clean.cmake         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/depend.make         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/flags.make         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/link.txt         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/linklibs.rsp         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/objects1.rsp         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/progress.make         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/progress.marks         new file:   MinAgeStaff/cmake-build-debug/Makefile         new file:   MinAgeStaff/cmake-build-debug/MinAgeStaff.cbp         new file:   MinAgeStaff/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   MinAgeStaff/cmake-build-debug/cmake_install.cmake         new file:   MinAgeStaff/main.c

--- MatrixMaX/main.c
+++ MatrixMaX/main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
-
+void MatrixMax(void);
 int main() {
-    printf("Hello, World!\n");
+    MatrixMax();
     return 0;
+}
+void MatrixMax(void){
+//    FILE *fp_in;
+//    fp_in=fopen("../data.txt","r");
+//    if (fp_in==NULL){
+//        printf("can't open the file");
+//    }
+    int row,col;
+    while (scanf("%d %d",&row,&col)!=EOF){
+        int matrix[row][col];
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                scanf("%d",&matrix[i][j]);
+            }
+        }
+//        for (int i = 0; i < row; ++i) {
+//            for (int j = 0; j < col; ++j) {
+//                printf("%d ",matrix[i][j]);
+//            }
+//            printf("\n");
+//        }
+        for (int i = 0; i < row; ++i) {
+            int max=matrix[i][0];
+            int place=0;
+            int sum=0;
+            for (int j = 0; j < col; ++j) {
+                if (matrix[i][j]>max){
+                    max=matrix[i][j];
+                    place=j;
+                }
+                sum=sum+matrix[i][j];
+            }
+            matrix[i][place]=sum;
+        }
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                printf("%d ",matrix[i][j]);
+            }
+            printf("\n");
+        }
+    }
 }
---
+++ MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinAgeStaff.dir/main.c.obj"
+  "MinAgeStaff.exe"
+  "MinAgeStaff.exe.manifest"
+  "MinAgeStaff.pdb"
+  "libMinAgeStaff.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MinAgeStaff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
