07a42e9b1245f6a00812e3862e2402923b0d187d
xiaofangxiaoyao <2428300273@qq.com>
2021-02-02 18:48:12 +0800

    new file:   AscendLinkList/.idea/.gitignore         new file:   AscendLinkList/.idea/AscendLinkList.iml         new file:   AscendLinkList/.idea/misc.xml         new file:   AscendLinkList/.idea/modules.xml         new file:   AscendLinkList/.idea/vcs.xml         new file:   AscendLinkList/CMakeLists.txt         new file:   AscendLinkList/cmake-build-debug/AscendLinkList.cbp         new file:   AscendLinkList/cmake-build-debug/CMakeCache.txt         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/DependInfo.cmake         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/build.make         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/cmake_clean.cmake         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/depend.make         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/flags.make         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/link.txt         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/linklibs.rsp         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/objects1.rsp         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/progress.make         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/Makefile2         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   AscendLinkList/cmake-build-debug/CMakeFiles/progress.marks         new file:   AscendLinkList/cmake-build-debug/Makefile         new file:   AscendLinkList/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   AscendLinkList/cmake-build-debug/cmake_install.cmake         new file:   AscendLinkList/main.c         new file:   GradeSort/cmake-build-debug/CMakeFiles/GradeSort.dir/C.includecache         new file:   GradeSort/cmake-build-debug/CMakeFiles/GradeSort.dir/depend.internal         modified:   GradeSort/cmake-build-debug/CMakeFiles/GradeSort.dir/depend.make         new file:   GradeSort/cmake-build-debug/CMakeFiles/GradeSort.dir/main.c.obj         new file:   GradeSort/cmake-build-debug/CMakeFiles/GradeSort.dir/objects.a         new file:   GradeSort/cmake-build-debug/GradeSort.exe         modified:   GradeSort/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   GradeSort/main.c

---
+++ AscendLinkList/cmake-build-debug/CMakeFiles/AscendLinkList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AscendLinkList.exe"
+  "AscendLinkList.exe.manifest"
+  "AscendLinkList.pdb"
+  "CMakeFiles/AscendLinkList.dir/main.c.obj"
+  "libAscendLinkList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AscendLinkList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- GradeSort/main.c
+++ GradeSort/main.c
@@ -1,6 +1,105 @@
 #include <stdio.h>
+#include "string.h"
+#include "stdlib.h"
+typedef struct Student{
+    char name[101];
+    int age;
+    int grade;
+}Student;
+void GradeSort(void);
+Student CreateStudent(char name[100],int age,int grade);
+void InnerSort(struct Student *student, int number);
+void SortByGrade(struct Student *student, int number);
+void SortByName(struct Student *student, int number);
+void SortByAge(struct Student *student, int number);
+void PrintfAll(struct Student*student,int number);
+void Exchange(struct Student *student1,struct Student*student2);
+
+
 
 int main() {
-    printf("Hello, World!\n");
+    GradeSort();
     return 0;
+}
+void GradeSort(void){
+    int number;
+    scanf("%d",&number);
+    struct Student AllStudent[number];
+    for (int i = 0; i < number; ++i) {
+        char name[101];
+        int age;
+        int grade;
+        scanf("%s %d %d",name,&age,&grade);
+        AllStudent[i]=CreateStudent(name,age,grade);
+    }
+    InnerSort(AllStudent,number);
+    PrintfAll(AllStudent,number);
+}
+void PrintfAll(struct Student*student,int number){
+    for (int i = 0; i < number; ++i) {
+        printf("%s %d %d\n",student[i].name,student[i].age,student[i].grade);
+    }
+}
+void Exchange(struct Student *student1,struct Student*student2){
+    int grade,age;
+    char name[101];
+    grade=(*student1).grade;
+    (*student1).grade=(*student2).grade;
+    (*student2).grade=grade;
+    age=(*student1).age;
+    (*student1).age=(*student2).age;
+    (*student2).age=age;
+    for (int i = 0; i < 101; ++i) {
+        name[i]=(*student1).name[i];
+        (*student1).name[i]=(*student2).name[i];
+        (*student2).name[i]=name[i];
+    }
+}
+void SortByGrade(struct Student *student, int number){
+    for (int i = 0; i < number; ++i) {
+        for (int j = 1; j < number-i; ++j) {
+            if (student[j-1].grade>student[j].grade){
+                Exchange(&student[j-1],&student[j]);
+            }
+            if (student[j-1].grade==student[j].grade){
+                SortByName(student+j-1,2);
+            }
+        }
+    }
+}
+void SortByName(struct Student *student, int number){
+    for (int i = 0; i < number; ++i) {
+        for (int j = 1; j < number-i; ++j) {
+            if (strcmp(student[j-1].name,student[j].name)>0){//strcmp return :if str1>str2 >0
+                Exchange(&student[j-1],&student[j]);
+            }
+            if (strcmp(student[j-1].name,student[j].name)==0){
+                SortByAge(student+j-1,2);
+            }
+        }
+    }
+}
+void SortByAge(struct Student *student, int number){
+    for (int i = 0; i < number; ++i) {
+        for (int j = 1; j < number-i; ++j) {
+            if (student[j-1].age>student[j].age){
+                Exchange(&student[j-1],&student[j]);
+            }
+        }
+    }
+}
+void InnerSort(struct Student *student, int number){
+    SortByGrade(student,number);
+//    SortByName(student,number);
+//    SortByAge(student,number);
+}
+
+Student  CreateStudent(char name[101],int age,int grade){
+    struct Student * student=(struct Student*)malloc(sizeof (struct Student));
+    for (int i = 0; i < 101; ++i) {
+        student->name[i]=name[i];
+    }
+    student->age=age;
+    student->grade=grade;
+    return *student;
 }
