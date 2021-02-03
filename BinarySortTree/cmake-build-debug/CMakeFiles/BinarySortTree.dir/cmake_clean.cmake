7026c5dd21e4e54380f1df3f97ce22cb05cd5ee2
xiaofangxiaoyao <2428300273@qq.com>
2021-02-03 15:05:20 +0800

    new file:   BinarySortTree/.idea/.gitignore         new file:   BinarySortTree/.idea/BinarySortTree.iml         new file:   BinarySortTree/.idea/misc.xml         new file:   BinarySortTree/.idea/modules.xml         new file:   BinarySortTree/.idea/vcs.xml         new file:   BinarySortTree/CMakeLists.txt         new file:   BinarySortTree/cmake-build-debug/BinarySortTree.cbp         new file:   BinarySortTree/cmake-build-debug/CMakeCache.txt         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/DependInfo.cmake         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/build.make         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/cmake_clean.cmake         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/depend.make         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/flags.make         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/link.txt         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/linklibs.rsp         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/objects1.rsp         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/progress.make         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/Makefile2         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   BinarySortTree/cmake-build-debug/CMakeFiles/progress.marks         new file:   BinarySortTree/cmake-build-debug/Makefile         new file:   BinarySortTree/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   BinarySortTree/cmake-build-debug/cmake_install.cmake         new file:   BinarySortTree/main.c         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/C.includecache         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/depend.internal         modified:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/depend.make         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/main.c.obj         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/objects.a         new file:   PrintDay/cmake-build-debug/PrintDay.exe         modified:   PrintDay/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   PrintDay/main.c

---
+++ BinarySortTree/cmake-build-debug/CMakeFiles/BinarySortTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BinarySortTree.exe"
+  "BinarySortTree.exe.manifest"
+  "BinarySortTree.pdb"
+  "CMakeFiles/BinarySortTree.dir/main.c.obj"
+  "libBinarySortTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BinarySortTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- PrintDay/main.c
+++ PrintDay/main.c
@@ -1,6 +1,169 @@
 #include <stdio.h>
+void PrintDAY(void);
+int IsLeapYear(int year);
+void PrintLeap(int year,int day);
+void PrintNoLeap(int year,int day);
 
 int main() {
-    printf("Hello, World!\n");
+    PrintDAY();
     return 0;
+}
+int IsLeapYear(int year){
+    int isleap=0;
+    if (year%4==0){
+        if (year%100!=0){
+            isleap=1;
+        } else{
+            if (year%400==0){
+                isleap=1;
+            }
+        }
+    }
+    return isleap;
+}
+void PrintDAY(void){
+    int year,day;
+    while (scanf("%d %d",&year,&day)!=EOF){
+        if (IsLeapYear(year)){
+            PrintLeap(year,day);
+        } else{
+            PrintNoLeap(year,day);
+        }
+    }
+}
+void PrintLeap(int year,int day){
+    //1 3 5 7 8 10 12:31
+    //2:29
+    //4 6 9 11:30
+    int January_first=1;
+    int February_first=January_first+31;
+    int March_first=February_first+29;
+    int April_first=March_first+31;
+    int May_first=April_first+30;
+    int June_first=May_first+31;
+    int july_first=June_first+30;
+    int August_first=july_first+31;
+    int September_first=August_first+31;
+    int October_first=September_first+30;
+    int November_first=October_first+31;
+    int December_first=November_first+30;
+    int month;
+    int month_day;
+    if (day>=January_first&&day<February_first){
+        month=1;
+        month_day=day-January_first+1;
+    }
+    if (day>=February_first&&day<March_first){
+        month=2;
+        month_day=day-February_first+1;
+    }
+    if (day>=March_first&&day<April_first){
+        month=3;
+        month_day=day-March_first+1;
+    }
+    if (day>=April_first&&day<May_first){
+        month=4;
+        month_day=day-April_first+1;
+    }
+    if (day>=May_first&&day<June_first){
+        month=5;
+        month_day=day-May_first+1;
+    }
+    if (day>=June_first&&day<july_first){
+        month=6;
+        month_day=day-June_first+1;
+    }
+    if (day>=july_first&&day<August_first){
+        month=7;
+        month_day=day-july_first+1;
+    }
+    if (day>=August_first&&day<September_first){
+        month=8;
+        month_day=day-August_first+1;
+    }
+    if (day>=September_first&&day<October_first){
+        month=9;
+        month_day=day-September_first+1;
+    }
+    if (day>=October_first&&day<November_first){
+        month=10;
+        month_day=day-October_first+1;
+    }
+    if (day>=November_first&&day<December_first){
+        month=11;
+        month_day=day-November_first+1;
+    }
+    if (day>=December_first){
+        month=12;
+        month_day=day-December_first+1;
+    }
+    printf("%d-%02d-%02d\n",year,month,month_day);
+}
+void PrintNoLeap(int year,int day){
+    //1 3 5 7 8 10 12:31
+    //2:29
+    //4 6 9 11:30
+    int January_first=1;
+    int February_first=January_first+31;
+    int March_first=February_first+28;
+    int April_first=March_first+31;
+    int May_first=April_first+30;
+    int June_first=May_first+31;
+    int july_first=June_first+30;
+    int August_first=july_first+31;
+    int September_first=August_first+31;
+    int October_first=September_first+30;
+    int November_first=October_first+31;
+    int December_first=November_first+30;
+    int month;
+    int month_day;
+    if (day>=January_first&&day<February_first){
+        month=1;
+        month_day=day-January_first+1;
+    }
+    if (day>=February_first&&day<March_first){
+        month=2;
+        month_day=day-February_first+1;
+    }
+    if (day>=March_first&&day<April_first){
+        month=3;
+        month_day=day-March_first+1;
+    }
+    if (day>=April_first&&day<May_first){
+        month=4;
+        month_day=day-April_first+1;
+    }
+    if (day>=May_first&&day<June_first){
+        month=5;
+        month_day=day-May_first+1;
+    }
+    if (day>=June_first&&day<july_first){
+        month=6;
+        month_day=day-June_first+1;
+    }
+    if (day>=july_first&&day<August_first){
+        month=7;
+        month_day=day-july_first+1;
+    }
+    if (day>=August_first&&day<September_first){
+        month=8;
+        month_day=day-August_first+1;
+    }
+    if (day>=September_first&&day<October_first){
+        month=9;
+        month_day=day-September_first+1;
+    }
+    if (day>=October_first&&day<November_first){
+        month=10;
+        month_day=day-October_first+1;
+    }
+    if (day>=November_first&&day<December_first){
+        month=11;
+        month_day=day-November_first+1;
+    }
+    if (day>=December_first){
+        month=12;
+        month_day=day-December_first+1;
+    }
+    printf("%d-%02d-%02d\n",year,month,month_day);
 }
