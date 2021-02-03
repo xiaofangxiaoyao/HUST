8b522508dca4e9cd696d600bf7f33cf15afe891b
xiaofangxiaoyao <2428300273@qq.com>
2021-02-03 18:33:25 +0800

    new file:   FindRepeatChar/.idea/FindRepeatChar.iml         new file:   FindRepeatChar/.idea/misc.xml         new file:   FindRepeatChar/.idea/modules.xml         new file:   FindRepeatChar/.idea/vcs.xml         new file:   FindRepeatChar/.idea/workspace.xml         new file:   FindRepeatChar/CMakeLists.txt         new file:   FindRepeatChar/cmake-build-debug/CMakeCache.txt         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/DependInfo.cmake         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/build.make         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/cmake_clean.cmake         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/depend.make         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/flags.make         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/link.txt         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/linklibs.rsp         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/objects1.rsp         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/progress.make         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/Makefile2         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/progress.marks         new file:   FindRepeatChar/cmake-build-debug/FindRepeatChar.cbp         new file:   FindRepeatChar/cmake-build-debug/Makefile         new file:   FindRepeatChar/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   FindRepeatChar/cmake-build-debug/cmake_install.cmake         new file:   FindRepeatChar/main.c         modified:   PalindromeString/main.c

---
+++ FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FindRepeatChar.dir/main.c.obj"
+  "FindRepeatChar.exe"
+  "FindRepeatChar.exe.manifest"
+  "FindRepeatChar.pdb"
+  "libFindRepeatChar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FindRepeatChar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- PalindromeString/main.c
+++ PalindromeString/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-
+#include "string.h"
 int main() {
-    printf("Hello, World!\n");
+    char s[1001];
+    int len=0;
+    while (scanf("%s",s)!=EOF){
+        len=strlen(s);
+        int flag=1;
+        for (int i = 0 ; i < len; ++i) {
+            if (s[i]!=s[len-i-1]){
+                flag=0;
+                break;;
+            }
+        }
+        if (flag){
+            printf("Yes!\n");
+        } else{
+            printf("No!\n");
+        }
+    }
     return 0;
 }
