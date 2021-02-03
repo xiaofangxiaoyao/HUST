e1a053e3bfbbf5147c260bcea3e7b6c10ed11443
xiaofangxiaoyao <2428300273@qq.com>
2021-02-03 10:59:20 +0800

    modified:   BinaryOrderTree/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   MinAgeStaff/.idea/workspace.xml         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/C.includecache         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/depend.internal         modified:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/depend.make         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/main.c.obj         new file:   MinAgeStaff/cmake-build-debug/CMakeFiles/MinAgeStaff.dir/objects.a         new file:   MinAgeStaff/cmake-build-debug/MinAgeStaff.exe         modified:   MinAgeStaff/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   MinAgeStaff/data.txt         modified:   MinAgeStaff/main.c         new file:   SymmetricMatrix/.idea/.gitignore         new file:   SymmetricMatrix/.idea/SymmetricMatrix.iml         new file:   SymmetricMatrix/.idea/misc.xml         new file:   SymmetricMatrix/.idea/modules.xml         new file:   SymmetricMatrix/.idea/vcs.xml         new file:   SymmetricMatrix/CMakeLists.txt         new file:   SymmetricMatrix/cmake-build-debug/CMakeCache.txt         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/Makefile2         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/DependInfo.cmake         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/build.make         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/cmake_clean.cmake         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/depend.make         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/flags.make         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/link.txt         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/linklibs.rsp         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/objects1.rsp         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/progress.make         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   SymmetricMatrix/cmake-build-debug/CMakeFiles/progress.marks         new file:   SymmetricMatrix/cmake-build-debug/Makefile         new file:   SymmetricMatrix/cmake-build-debug/SymmetricMatrix.cbp         new file:   SymmetricMatrix/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   SymmetricMatrix/cmake-build-debug/cmake_install.cmake         new file:   SymmetricMatrix/main.c

--- MinAgeStaff/main.c
+++ MinAgeStaff/main.c
@@ -1,6 +1,118 @@
 #include <stdio.h>
+#include "stdlib.h"
+#include "string.h"
+void MinAgeStuff(void);
+typedef struct Staff{
+    int number;
+    char name[11];
+    int age;
+}Staff;
+struct Staff* CreateStaff(int number,char *name,int age);
 
 int main() {
-    printf("Hello, World!\n");
+    MinAgeStuff();
     return 0;
+}
+struct Staff* CreateStaff(int number,char *name,int age){
+    Staff* staff=(struct Staff*)malloc(sizeof (struct Staff));//malloc never forget the(struct Staff*)
+    staff->age=age;
+    staff->number=number;
+    int i=0;
+//    printf("name[%d]=%c\n",i,name[i]);
+    for (i = 0; name[i]!='\0'; ++i) {
+        staff->name[i]=name[i];
+//        printf("name[%d]=%c\n",i,name[i]);
+//        printf("%c",staff->name[i]);
+    }
+    staff->name[i]='\0';
+    return staff;
+}
+void MinAgeStuff(void){
+//    FILE *fp_in;
+//    fp_in=fopen("../data.txt","r");
+//    if (fp_in==NULL){
+//        printf("can't open the file");
+//    }
+    int staff_number;
+    while (scanf("%d",&staff_number)!=EOF){
+        int number;
+        char name[11];
+        int age;
+        struct Staff* AllStaff[staff_number];//seem like not really understand struct
+        for (int i = 0; i < staff_number; ++i) {
+            scanf("%d %s %d",&number,name,&age);
+            AllStaff[i]=CreateStaff(number,name,age);
+//            printf("%s\n",AllStaff[i]->name);
+        }
+//        for (int i = 0; i < staff_number; ++i) {
+//            printf("%d %s %d\n",AllStaff[i]->number,AllStaff[i]->name,AllStaff[i]->age);
+//        }
+        int first,second,third;
+        first=0;
+        int first_min=1000;
+        for (int i = 0; i < staff_number; ++i) {
+            if (AllStaff[i]->age<first_min){
+                first_min=AllStaff[i]->age;
+                first=i;
+            }
+            if (AllStaff[i]->age==first_min)
+            {
+                if (AllStaff[i]->number<AllStaff[first]->number){
+                    first_min=AllStaff[i]->age;
+                    first=i;
+                }
+                if (AllStaff[i]->number==AllStaff[first]->number){
+                    if (strcmp(AllStaff[i]->name,AllStaff[first]->name)<0){
+                        first_min=AllStaff[i]->age;
+                        first=i;
+                    }
+                }
+            }
+        }
+//        printf("%d",first);
+        printf("%d %s %d\n",AllStaff[first]->number,AllStaff[first]->name,AllStaff[first]->age);
+        int second_min=1000;
+        for (int i = 0; i < staff_number; ++i) {
+            if (AllStaff[i]->age<second_min&&AllStaff[i]->age>=first_min&&i!=first){
+                second_min=AllStaff[i]->age;
+                second=i;
+            }
+            if (AllStaff[i]->age==second_min)
+            {
+                if (AllStaff[i]->number<AllStaff[second]->number){
+                    second_min=AllStaff[i]->age;
+                    second=i;
+                }
+                if (AllStaff[i]->number==AllStaff[second]->number){
+                    if (strcmp(AllStaff[i]->name,AllStaff[second]->name)<0){
+                        second_min=AllStaff[i]->age;
+                        second=i;
+                    }
+                }
+            }
+        }
+        printf("%d %s %d\n",AllStaff[second]->number,AllStaff[second]->name,AllStaff[second]->age);
+        int third_min=1000;
+        for (int i = 0; i < staff_number; ++i) {
+            if (AllStaff[i]->age<third_min&&i!=first&&i!=second&&AllStaff[i]->age>=second_min){
+                third_min=AllStaff[i]->age;
+                third=i;
+            }
+            if (AllStaff[i]->age==third_min)
+            {
+                if (AllStaff[i]->number<AllStaff[third]->number){
+                    third_min=AllStaff[i]->age;
+                    third=i;
+                }
+                if (AllStaff[i]->number==AllStaff[third]->number){
+                    if (strcmp(AllStaff[i]->name,AllStaff[third]->name)<0){
+                        third_min=AllStaff[i]->age;
+                        third=i;
+                    }
+                }
+            }
+        }
+        printf("%d %s %d",AllStaff[third]->number,AllStaff[third]->name,AllStaff[third]->age);
+
+    }
 }
---
+++ SymmetricMatrix/cmake-build-debug/CMakeFiles/SymmetricMatrix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SymmetricMatrix.dir/main.c.obj"
+  "SymmetricMatrix.exe"
+  "SymmetricMatrix.exe.manifest"
+  "SymmetricMatrix.pdb"
+  "libSymmetricMatrix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SymmetricMatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
