f10a98f49c77df3a47b5424d2dec5c7527d60af9
xiaofangxiaoyao <2428300273@qq.com>
2021-02-03 13:20:02 +0800

    modified:   A+B/.idea/workspace.xml         new file:   A+B/cmake-build-debug/A_B.exe         new file:   A+B/cmake-build-debug/CMakeFiles/A_B.dir/C.includecache         new file:   A+B/cmake-build-debug/CMakeFiles/A_B.dir/depend.internal         modified:   A+B/cmake-build-debug/CMakeFiles/A_B.dir/depend.make         new file:   A+B/cmake-build-debug/CMakeFiles/A_B.dir/main.c.obj         new file:   A+B/cmake-build-debug/CMakeFiles/A_B.dir/objects.a         modified:   A+B/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   A+B/data.txt         modified:   A+B/main.c         new file:   PrintDay/.idea/.gitignore         new file:   PrintDay/.idea/PrintDay.iml         new file:   PrintDay/.idea/misc.xml         new file:   PrintDay/.idea/modules.xml         new file:   PrintDay/.idea/vcs.xml         new file:   PrintDay/CMakeLists.txt         new file:   PrintDay/cmake-build-debug/CMakeCache.txt         new file:   PrintDay/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   PrintDay/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   PrintDay/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   PrintDay/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   PrintDay/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   PrintDay/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   PrintDay/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   PrintDay/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   PrintDay/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   PrintDay/cmake-build-debug/CMakeFiles/Makefile2         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/DependInfo.cmake         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/build.make         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/cmake_clean.cmake         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/depend.make         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/flags.make         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/link.txt         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/linklibs.rsp         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/objects1.rsp         new file:   PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/progress.make         new file:   PrintDay/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   PrintDay/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   PrintDay/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   PrintDay/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   PrintDay/cmake-build-debug/CMakeFiles/progress.marks         new file:   PrintDay/cmake-build-debug/Makefile         new file:   PrintDay/cmake-build-debug/PrintDay.cbp         new file:   PrintDay/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   PrintDay/cmake-build-debug/cmake_install.cmake         new file:   PrintDay/main.c “

--- A+B/main.c
+++ A+B/main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
-
+#include "stdlib.h"
+#include "string.h"
+void Add(void);
 int main() {
-    printf("Hello, World!\n");
+    Add();
     return 0;
+}
+void Add(void){
+//    FILE* fp;
+//    fp=fopen("../data.txt","r");
+    char array[2][100][20];
+    char in[2][20];
+    int sum[3];
+    while ((scanf("%s",in[0])!=EOF)&&scanf("%s",in[1])!=EOF){
+        int first,second;
+        for (int i = 0; i < 2; ++i) {
+            char *p;
+            p=strtok(in[i],",");
+            int j=0;
+            while (p!=NULL){
+                strcpy(array[i][j],p);
+                j++;
+                p=strtok(NULL,",");
+            }
+            if (i==0){
+                first=j;
+            } else{
+                second=j;
+            }
+        }
+//        for (int i = 0; i < 2; ++i) {
+//            for (int j = 0; j < 3; ++j) {
+//                printf("%s\n",array[i][j]);
+//            }
+//        }
+        char number[2][20];
+        for (int i = 0; i < 2; ++i) {
+            char *temp=array[i][0];
+            int n;
+            if (i==0){
+                n=first;
+            } else{
+                n=second;
+            }
+            for (int j = 1; j < n; ++j) {
+                strcat(temp,array[i][j]);
+//                printf("temp=%s\n",temp);
+            }
+            strcpy(number[i],temp);
+        }
+//        for (int i = 0; i < 2; ++i) {
+//            printf("number[%d]=%s\n",i,number[i]);
+//        }
+        printf("%d\n",atoi(number[0])+atoi(number[1]));
+    }
+
 }
---
+++ PrintDay/cmake-build-debug/CMakeFiles/PrintDay.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrintDay.dir/main.c.obj"
+  "PrintDay.exe"
+  "PrintDay.exe.manifest"
+  "PrintDay.pdb"
+  "libPrintDay.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrintDay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
