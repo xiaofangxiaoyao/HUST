af8dd4a8071fdcd410682fb330a0002008e8b832
xiaofangxiaoyao <2428300273@qq.com>
2021-02-03 21:58:28 +0800

    modified:   Factorial/.idea/Factorial.iml         new file:   Factorial/cmake-build-debug/CMakeFiles/Factorial.dir/C.includecache         new file:   Factorial/cmake-build-debug/CMakeFiles/Factorial.dir/depend.internal         modified:   Factorial/cmake-build-debug/CMakeFiles/Factorial.dir/depend.make         new file:   Factorial/cmake-build-debug/CMakeFiles/Factorial.dir/main.c.obj         new file:   Factorial/cmake-build-debug/CMakeFiles/Factorial.dir/objects.a         new file:   Factorial/cmake-build-debug/Factorial.exe         modified:   Factorial/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   Factorial/main.c         modified:   FindRepeatChar/.idea/workspace.xml         new file:   Octal/.idea/.gitignore         new file:   Octal/.idea/Octal.iml         new file:   Octal/.idea/misc.xml         new file:   Octal/.idea/modules.xml         new file:   Octal/.idea/vcs.xml         new file:   Octal/CMakeLists.txt         new file:   Octal/cmake-build-debug/CMakeCache.txt         new file:   Octal/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   Octal/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   Octal/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   Octal/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   Octal/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   Octal/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   Octal/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   Octal/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   Octal/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   Octal/cmake-build-debug/CMakeFiles/Makefile2         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/DependInfo.cmake         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/build.make         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/cmake_clean.cmake         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/depend.make         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/flags.make         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/link.txt         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/linklibs.rsp         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/objects1.rsp         new file:   Octal/cmake-build-debug/CMakeFiles/Octal.dir/progress.make         new file:   Octal/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   Octal/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   Octal/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   Octal/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   Octal/cmake-build-debug/CMakeFiles/progress.marks         new file:   Octal/cmake-build-debug/Makefile         new file:   Octal/cmake-build-debug/Octal.cbp         new file:   Octal/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   Octal/cmake-build-debug/cmake_install.cmake         new file:   Octal/main.c

--- Factorial/main.c
+++ Factorial/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int number;
+    scanf("%d",&number);
+    int odd,even;
+    if(number%2==0){
+        even=number;
+        odd=number-1;
+    } else{
+        odd=number;
+        even=number-1;
+
+    }
+    int array[number+1];
+    array[0]=1;
+    for (int i = 1; i < number+1; i=i+1) {
+        array[i]=array[i-1]*i;
+    }
+    int odd_sum,evensum;
+    odd_sum=0;
+    evensum=0;
+    for (int i = 1; i <odd+2 ; i=i+2) {
+        odd_sum=array[i]+odd_sum;
+    }
+    for (int i = 2; i <even+2 ; i=i+2) {
+        evensum=array[i]+evensum;
+    }
+    printf("%d %d",odd_sum,evensum);
     return 0;
 }
---
+++ Octal/cmake-build-debug/CMakeFiles/Octal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Octal.dir/main.c.obj"
+  "Octal.exe"
+  "Octal.exe.manifest"
+  "Octal.pdb"
+  "libOctal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Octal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
