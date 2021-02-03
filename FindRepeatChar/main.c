a05a76f429d5b1bf41d36391427ba792ee5609f5
xiaofangxiaoyao <2428300273@qq.com>
2021-02-03 20:13:46 +0800

    modified:   FindRepeatChar/.idea/FindRepeatChar.iml         modified:   FindRepeatChar/.idea/workspace.xml         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/C.includecache         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/depend.internal         modified:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/depend.make         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/main.c.obj         new file:   FindRepeatChar/cmake-build-debug/CMakeFiles/FindRepeatChar.dir/objects.a         new file:   FindRepeatChar/cmake-build-debug/FindRepeatChar.exe         modified:   FindRepeatChar/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   FindRepeatChar/main.c

--- FindRepeatChar/main.c
+++ FindRepeatChar/main.c
@@ -1,6 +1,93 @@
 #include <stdio.h>
-
+#include "string.h"
+#include "stdlib.h"
+typedef struct Node{
+    char a;
+    int n;
+    int ps[200];
+}Node;
+struct Node* CreateNode(char a);
+int search(struct Node nodearray[100],int node_number,char c);
+int HasNode(struct Node nodearray[100],int node_number,char c);
 int main() {
-    printf("Hello, World!\n");
+    char s[1000];
+    struct Node * nodearray[100];
+    int node_number=0;
+    scanf("%s",s);
+    int len=strlen(s);
+    int place=0;
+    for (int i = 0; i < len; ++i) {
+        if (i==0){
+            nodearray[0]=CreateNode(s[i]);
+            nodearray[0]->ps[nodearray[0]->n]=i;
+            nodearray[0]->n++;
+            node_number++;
+
+        } else{
+            int flag=0;
+            for (int j = 0; j < node_number; ++j) {
+                if (nodearray[j]->a==s[i]){
+                    flag=1;
+                    break;
+                }
+            }
+            if (flag){
+                int place=0;
+                for (int j = 0; j < node_number; ++j) {
+                    if ((nodearray[j])->a==s[i]){
+                        place=j;
+                        break;
+                    }
+                }
+                nodearray[place]->ps[nodearray[place]->n]=i;
+                nodearray[place]->n++;
+            } else{
+                nodearray[node_number]=CreateNode(s[i]);
+                nodearray[node_number]->ps[nodearray[node_number]->n]=i;
+                nodearray[node_number]->n++;
+                node_number++;
+            }
+        }
+    }
+    for (int i = 0; i < node_number; ++i) {
+        if (nodearray[i]->n>1){
+            for (int j = 0; j < nodearray[i]->n; ++j) {
+                printf("%c:%d",nodearray[i]->a,nodearray[i]->ps[j]);
+                if (j!=nodearray[i]->n-1){
+                    printf(",");
+                }
+            }
+            printf("\n");
+        }
+    }
     return 0;
+}
+struct Node* CreateNode(char a){
+    struct Node* node=(struct Node*)malloc(sizeof (struct Node));
+    node->a=a;
+    node->n=0;
+    for (int i = 0; i < 200; ++i) {
+        node->ps[i]=0;
+    }
+    return node;
+}
+int search(struct Node nodearray[100],int node_number,char c){//try to find out how to pass nodearray
+    int place=0;
+    for (int i = 0; i < node_number; ++i) {
+        if ((&nodearray[i])->a==c){
+            place=i;
+            break;
+        }
+    }
+    return place;
+}
+int HasNode(struct Node nodearray[100],int node_number,char c){
+    int flag=0;
+    for (int i = 0; i < node_number; ++i) {
+        if ((&nodearray[i])->a==c){
+            flag=1;
+            break;
+        }
+    }
+    return flag;
 }
