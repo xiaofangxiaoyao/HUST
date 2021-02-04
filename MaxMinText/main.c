2c2f7ca27b8cdd99252bfa0382018471ec142943
xiaofangxiaoyao <2428300273@qq.com>
2021-02-04 11:35:31 +0800

    new file:   Farmer/.idea/Farmer.iml         new file:   Farmer/.idea/misc.xml         new file:   Farmer/.idea/modules.xml         new file:   Farmer/.idea/vcs.xml         new file:   Farmer/.idea/workspace.xml         new file:   Farmer/CMakeLists.txt         new file:   Farmer/cmake-build-debug/CMakeCache.txt         new file:   Farmer/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   Farmer/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   Farmer/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   Farmer/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   Farmer/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   Farmer/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   Farmer/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   Farmer/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/DependInfo.cmake         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/build.make         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/cmake_clean.cmake         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/depend.make         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/flags.make         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/link.txt         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/linklibs.rsp         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/objects1.rsp         new file:   Farmer/cmake-build-debug/CMakeFiles/Farmer.dir/progress.make         new file:   Farmer/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   Farmer/cmake-build-debug/CMakeFiles/Makefile2         new file:   Farmer/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   Farmer/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   Farmer/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   Farmer/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   Farmer/cmake-build-debug/CMakeFiles/progress.marks         new file:   Farmer/cmake-build-debug/Farmer.cbp         new file:   Farmer/cmake-build-debug/Makefile         new file:   Farmer/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   Farmer/cmake-build-debug/cmake_install.cmake         new file:   Farmer/main.c         new file:   MaxMinText/cmake-build-debug/CMakeFiles/MaxMinText.dir/C.includecache         new file:   MaxMinText/cmake-build-debug/CMakeFiles/MaxMinText.dir/depend.internal         modified:   MaxMinText/cmake-build-debug/CMakeFiles/MaxMinText.dir/depend.make         new file:   MaxMinText/cmake-build-debug/CMakeFiles/MaxMinText.dir/main.c.obj         new file:   MaxMinText/cmake-build-debug/CMakeFiles/MaxMinText.dir/objects.a         new file:   MaxMinText/cmake-build-debug/MaxMinText.exe         modified:   MaxMinText/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   MaxMinText/data.txt         modified:   MaxMinText/main.c

--- MaxMinText/main.c
+++ MaxMinText/main.c
@@ -1,6 +1,67 @@
 #include <stdio.h>
-
+#include "string.h"
+#include "stdlib.h"
+typedef struct Node{
+    char s[400];
+    int len;
+}Node;
+struct Node* CreateNode(char *s,int len);
 int main() {
-    printf("Hello, World!\n");
+//    FILE *fp;
+//    fp=fopen("../data.txt","r");
+    char s[400];
+    struct Node* NodeArray[100];
+    int len=0;
+    int i=0;
+    while (gets(s)!=NULL){
+        len=strlen(s);
+//        printf("len= %d\n",len);
+        NodeArray[i]=CreateNode(s,len);
+        i++;
+        if (i>99){
+            printf("NodeArray out of bound");
+            break;
+        }
+    }
+    int NodeNumber=i;
+    int max_a,min_b;
+    max_a=min_b=NodeArray[0]->len;
+    int max[100];
+    int min[100];
+    for (int j = 1; j < NodeNumber; ++j) {
+        if (NodeArray[j]->len>max_a){
+            max_a=NodeArray[j]->len;
+        }
+        if (NodeArray[j]->len<min_b){
+            min_b=NodeArray[j]->len;
+        }
+    }
+    int max_len=0;
+    int min_len=0;
+    for (int j = 0; j < NodeNumber; ++j) {
+        if (NodeArray[j]->len==max_a){
+            max[max_len]=j;
+            max_len++;
+        }
+        if (NodeArray[j]->len==min_b){
+            min[min_len]=j;
+            min_len++;
+        }
+    }
+    for (int j = 0; j < min_len; ++j) {
+        printf("%s",NodeArray[min[j]]->s);
+        printf("\n");
+    }
+
+    for (int j = 0; j <max_len ; ++j) {
+        printf("%s",NodeArray[max[j]]->s);
+        printf("\n");
+    }
     return 0;
+}
+struct Node* CreateNode(char *s,int len){
+    struct Node* node =(struct Node*)malloc(sizeof (struct Node));
+    strcpy(node->s,s);
+    node->len=len;
+    return node;
 }
