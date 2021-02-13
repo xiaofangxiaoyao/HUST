c3d54fb14387115eaeb88c94c76084a67dadd361
xiaofangxiaoyao <2428300273@qq.com>
2021-02-13 11:54:06 +0800

    new file:   FILEjiaMI/.idea/FILEjiaMI.iml         new file:   FILEjiaMI/.idea/misc.xml         new file:   FILEjiaMI/.idea/modules.xml         new file:   FILEjiaMI/.idea/vcs.xml         new file:   FILEjiaMI/.idea/workspace.xml         new file:   FILEjiaMI/CMakeLists.txt         new file:   FILEjiaMI/cmake-build-debug/CMakeCache.txt         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/DependInfo.cmake         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/build.make         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/cmake_clean.cmake         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/depend.make         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/flags.make         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/link.txt         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/linklibs.rsp         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/objects1.rsp         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/progress.make         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/Makefile2         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   FILEjiaMI/cmake-build-debug/CMakeFiles/progress.marks         new file:   FILEjiaMI/cmake-build-debug/FILEjiaMI.cbp         new file:   FILEjiaMI/cmake-build-debug/Makefile         new file:   FILEjiaMI/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   FILEjiaMI/cmake-build-debug/cmake_install.cmake         new file:   FILEjiaMI/main.c         modified:   ReadFile/.idea/ReadFile.iml         new file:   ReadFile/abc.c         new file:   ReadFile/cmake-build-debug/CMakeFiles/ReadFile.dir/C.includecache         new file:   ReadFile/cmake-build-debug/CMakeFiles/ReadFile.dir/depend.internal         modified:   ReadFile/cmake-build-debug/CMakeFiles/ReadFile.dir/depend.make         new file:   ReadFile/cmake-build-debug/CMakeFiles/ReadFile.dir/main.c.obj         new file:   ReadFile/cmake-build-debug/CMakeFiles/ReadFile.dir/objects.a         new file:   ReadFile/cmake-build-debug/ReadFile.exe         modified:   ReadFile/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   ReadFile/main.c

---
+++ FILEjiaMI/cmake-build-debug/CMakeFiles/FILEjiaMI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FILEjiaMI.dir/main.c.obj"
+  "FILEjiaMI.exe"
+  "FILEjiaMI.exe.manifest"
+  "FILEjiaMI.pdb"
+  "libFILEjiaMI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FILEjiaMI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ReadFile/main.c
+++ ReadFile/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *fp;
+    fp=fopen("../abc.c","w+");
+    if (fp==NULL){
+        printf("can not read the file");
+    }
+    char s[200];
+    int i=1;
+    while (fgets(s,200,fp)!=NULL){
+        printf("%d %s\n",i,s);
+        i++;
+    }
     return 0;
 }
