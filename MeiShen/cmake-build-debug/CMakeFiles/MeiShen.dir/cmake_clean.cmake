7cff7fc582e96863a195a17f3cae00115a2b5594
xiaofangxiaoyao <2428300273@qq.com>
2021-02-13 11:17:30 +0800

    modified:   Farmer/.idea/workspace.xml         modified:   Farmer/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   MeiShen/.idea/.gitignore         new file:   MeiShen/.idea/MeiShen.iml         new file:   MeiShen/.idea/misc.xml         new file:   MeiShen/.idea/modules.xml         new file:   MeiShen/.idea/vcs.xml         new file:   MeiShen/CMakeLists.txt         new file:   MeiShen/cmake-build-debug/CMakeCache.txt         new file:   MeiShen/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   MeiShen/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   MeiShen/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   MeiShen/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   MeiShen/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   MeiShen/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   MeiShen/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   MeiShen/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   MeiShen/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   MeiShen/cmake-build-debug/CMakeFiles/Makefile2         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/DependInfo.cmake         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/build.make         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/cmake_clean.cmake         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/depend.make         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/flags.make         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/link.txt         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/linklibs.rsp         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/objects1.rsp         new file:   MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/progress.make         new file:   MeiShen/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   MeiShen/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   MeiShen/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   MeiShen/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   MeiShen/cmake-build-debug/CMakeFiles/progress.marks         new file:   MeiShen/cmake-build-debug/Makefile         new file:   MeiShen/cmake-build-debug/MeiShen.cbp         new file:   MeiShen/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   MeiShen/cmake-build-debug/cmake_install.cmake         new file:   MeiShen/main.c

---
+++ MeiShen/cmake-build-debug/CMakeFiles/MeiShen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MeiShen.dir/main.c.obj"
+  "MeiShen.exe"
+  "MeiShen.exe.manifest"
+  "MeiShen.pdb"
+  "libMeiShen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MeiShen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
