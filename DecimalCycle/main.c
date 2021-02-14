ba7050976d3291682c423260df6c59f57e03795d
xiaofangxiaoyao <2428300273@qq.com>
2021-02-14 10:26:31 +0800

    new file:   DecimalCycle/.idea/encodings.xml         modified:   DecimalCycle/.idea/workspace.xml         new file:   DecimalCycle/cmake-build-debug/CMakeFiles/DecimalCycle.dir/C.includecache         new file:   DecimalCycle/cmake-build-debug/CMakeFiles/DecimalCycle.dir/depend.internal         modified:   DecimalCycle/cmake-build-debug/CMakeFiles/DecimalCycle.dir/depend.make         new file:   DecimalCycle/cmake-build-debug/CMakeFiles/DecimalCycle.dir/main.c.obj         new file:   DecimalCycle/cmake-build-debug/CMakeFiles/DecimalCycle.dir/objects.a         new file:   DecimalCycle/cmake-build-debug/DecimalCycle.exe         modified:   DecimalCycle/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   DecimalCycle/main.c         modified:   ScannerInString/.idea/workspace.xml         new file:   StringPrintf/.idea/.gitignore         new file:   StringPrintf/.idea/StringPrintf.iml         new file:   StringPrintf/.idea/misc.xml         new file:   StringPrintf/.idea/modules.xml         new file:   StringPrintf/.idea/vcs.xml         new file:   StringPrintf/CMakeLists.txt         new file:   StringPrintf/cmake-build-debug/CMakeCache.txt         new file:   StringPrintf/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   StringPrintf/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   StringPrintf/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   StringPrintf/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   StringPrintf/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   StringPrintf/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   StringPrintf/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   StringPrintf/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   StringPrintf/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   StringPrintf/cmake-build-debug/CMakeFiles/Makefile2         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/DependInfo.cmake         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/build.make         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/cmake_clean.cmake         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/depend.make         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/flags.make         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/link.txt         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/linklibs.rsp         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/objects1.rsp         new file:   StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/progress.make         new file:   StringPrintf/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   StringPrintf/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   StringPrintf/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   StringPrintf/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   StringPrintf/cmake-build-debug/CMakeFiles/progress.marks         new file:   StringPrintf/cmake-build-debug/Makefile         new file:   StringPrintf/cmake-build-debug/StringPrintf.cbp         new file:   StringPrintf/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   StringPrintf/cmake-build-debug/cmake_install.cmake         new file:   StringPrintf/main.c

--- DecimalCycle/main.c
+++ DecimalCycle/main.c
@@ -1,37 +1,99 @@
-
 #include <stdio.h>
-#include <string.h>
-
-int main(int argc, char *argv[])
+#include <memory.h>
+#define MAX_LEN 1000
+int nume; //numerator分子
+int deno; //denominator分母
+int quot[MAX_LEN]; //quotient商
+int rem[MAX_LEN]; //remainder余数bai
+int negative_flag; //负数标志
+void save_quot_rem() //存取商和余数
 {
-    char num[128];
-    int nl, fl, ret = 0;
-    if (!*gets(num)) {
-        printf("input error!\n");
-        return -1;
+    int i;
+    negative_flag = nume/(double)deno < 0 ? 1 : 0; //若是负数则标志为1
+    nume = nume < 0 ? -nume : nume; //若是负数则转换为正数,下同
+    deno = deno < 0 ? -deno : deno;
+    for(i=0; i<MAX_LEN; i++)
+    {
+        quot[i] = nume/deno;
+        rem[i] = nume%deno;
+        nume = 10*rem[i];
+        if(!rem[i])
+            break;
+    } //for
+}
+//判断是否是循环小数,若是则保存循环节起始和结束位置
+int is_circu(int *start, int *end)
+{
+    int i, j;
+    for(i=0; i<MAX_LEN; i++)
+    {
+        if(-1 == rem[i])
+            return 0;
     }
-    nl = strlen(num);
-    for (fl = nl >> 1; fl; ) {
-        if (memcmp(num + nl - fl, num + nl - (fl << 1), fl)) {
-            fl--;
-        } else {
-            ret = fl;
-            if (fl & 1) {
-                break;
-            }
-            fl >>= 1;
-        }
-    }
-    if (!ret) {
-        printf("no factor found!\n");
-        return -2;
-    }
-    printf("factor is: %s", num + nl - ret);
+    for(i=0; i<MAX_LEN; i++)
+    {
+        for(j=i+1; j<MAX_LEN; j++)
+        {
+            if(rem[i] == rem[j])
+            {
+                *start = i;
+                *end = j;
+                return 1;
+            } //if
+        } //for
+    } //for
     return 0;
 }
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
+void show_circu(int start, int end) //显示循环小数
+{
+    int i;
+    printf(negative_flag ? "-%d." : "%d.", quot[0]); //整数部分
+    for(i=1; i<=start; i++) //小数非循环节部分
+    {
+        printf("%d", quot[i]);
+    }
+    printf("(");
+    for(i=start+1; i<=end; i++) //小数循环节部分
+    {
+        printf("%d", quot[i]);
+    }
+    printf(")\n\n");
+}
+void show_not_circu() //显示非循环小数
+{
+    int i;
+    if(-1 == quot[1]) //无小数部分
+    {
+        printf(negative_flag ? "-%d" : "%d", quot[0]);
+        printf("\n\n");
+        return;
+    }
+    printf(negative_flag ? "-%d." : "%d.", quot[0]);
+    for(i=1; (i < MAX_LEN) && (-1 != quot[i]); i++) //小数部分
+    {
+        printf("%d", quot[i]);
+    }
+    printf("\n\n");
+}
+int main()
+{
+    int start, end;
+    printf("请输入分子和分母,用空格分开(输入两个0退出):\n");
+    while(1)
+    {
+        fflush(stdin);
+        scanf("%d%d", &nume, &deno);
+        if(!nume && !deno)
+            return 0;
+        memset(quot, -1, sizeof(quot));
+        memset(rem, -1, sizeof(rem));
+        if(!deno)
+        {
+            printf("除数不能为0,请重新输入:\n");
+            continue;
+        }
+        save_quot_rem();
+        is_circu(&start, &end) ? show_circu(start, end) : show_not_circu();
+    }
     return 0;
 }
---
+++ StringPrintf/cmake-build-debug/CMakeFiles/StringPrintf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StringPrintf.dir/main.c.obj"
+  "StringPrintf.exe"
+  "StringPrintf.exe.manifest"
+  "StringPrintf.pdb"
+  "libStringPrintf.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StringPrintf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
