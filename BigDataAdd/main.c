3b4f570f073575816becbb3e52776d90ebf961b3
xiaofangxiaoyao <2428300273@qq.com>
2021-02-14 13:50:20 +0800

    new file:   BigDataAdd/cmake-build-debug/BigDataAdd.exe         new file:   BigDataAdd/cmake-build-debug/CMakeFiles/BigDataAdd.dir/C.includecache         new file:   BigDataAdd/cmake-build-debug/CMakeFiles/BigDataAdd.dir/depend.internal         modified:   BigDataAdd/cmake-build-debug/CMakeFiles/BigDataAdd.dir/depend.make         new file:   BigDataAdd/cmake-build-debug/CMakeFiles/BigDataAdd.dir/main.c.obj         new file:   BigDataAdd/cmake-build-debug/CMakeFiles/BigDataAdd.dir/objects.a         modified:   BigDataAdd/cmake-build-debug/Testing/Temporary/LastTest.log         modified:   BigDataAdd/main.c         new file:   JudgeScores/.idea/.gitignore         new file:   JudgeScores/.idea/JudgeScores.iml         new file:   JudgeScores/.idea/misc.xml         new file:   JudgeScores/.idea/modules.xml         new file:   JudgeScores/.idea/vcs.xml         new file:   JudgeScores/CMakeLists.txt         new file:   JudgeScores/cmake-build-debug/CMakeCache.txt         new file:   JudgeScores/cmake-build-debug/CMakeFiles/3.17.5/CMakeCCompiler.cmake         new file:   JudgeScores/cmake-build-debug/CMakeFiles/3.17.5/CMakeDetermineCompilerABI_C.bin         new file:   JudgeScores/cmake-build-debug/CMakeFiles/3.17.5/CMakeRCCompiler.cmake         new file:   JudgeScores/cmake-build-debug/CMakeFiles/3.17.5/CMakeSystem.cmake         new file:   JudgeScores/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/CMakeCCompilerId.c         new file:   JudgeScores/cmake-build-debug/CMakeFiles/3.17.5/CompilerIdC/a.exe         new file:   JudgeScores/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake         new file:   JudgeScores/cmake-build-debug/CMakeFiles/CMakeOutput.log         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/DependInfo.cmake         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/build.make         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/cmake_clean.cmake         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/depend.make         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/flags.make         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/link.txt         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/linklibs.rsp         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/objects1.rsp         new file:   JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/progress.make         new file:   JudgeScores/cmake-build-debug/CMakeFiles/Makefile.cmake         new file:   JudgeScores/cmake-build-debug/CMakeFiles/Makefile2         new file:   JudgeScores/cmake-build-debug/CMakeFiles/TargetDirectories.txt         new file:   JudgeScores/cmake-build-debug/CMakeFiles/clion-environment.txt         new file:   JudgeScores/cmake-build-debug/CMakeFiles/clion-log.txt         new file:   JudgeScores/cmake-build-debug/CMakeFiles/cmake.check_cache         new file:   JudgeScores/cmake-build-debug/CMakeFiles/progress.marks         new file:   JudgeScores/cmake-build-debug/JudgeScores.cbp         new file:   JudgeScores/cmake-build-debug/Makefile         new file:   JudgeScores/cmake-build-debug/Testing/Temporary/LastTest.log         new file:   JudgeScores/cmake-build-debug/cmake_install.cmake         new file:   JudgeScores/main.c         modified:   StringPress/cmake-build-debug/Testing/Temporary/LastTest.log

--- BigDataAdd/main.c
+++ BigDataAdd/main.c
@@ -1,6 +1,90 @@
 #include <stdio.h>
+#include "string.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char s1[2000];
+    char s2[2000];
+    while (scanf("%s",s1)!=EOF&& scanf("%s",s2)!=EOF){
+        int len1=strlen(s1);
+        int len2=strlen(s2);
+        int max_len;
+        if (len1==len2){
+            max_len=len1+1;
+            char s3[max_len+1];
+            int flag=0;
+            int j=len1-1;
+            for (int i = max_len-1; i >=1; --i) {
+                if (s1[j]+s2[j]+flag-2*'0'>=10){
+                    s3[i]=flag+s1[j]+s2[j]-'0'-10;
+                    flag=1;
+                } else{
+                    s3[i]=flag+s1[j]+s2[j]-'0';
+                    flag=0;
+                }
+                j--;
+            }
+            if (flag){
+                s3[0]='1';
+                s3[max_len]='\0';
+            } else{
+                max_len=max_len-1;
+                for (int i = 1; i <max_len+1; ++i) {
+                    s3[i-1]=s3[i];
+                }
+                s3[max_len]='\0';
+            }
+            printf("%s",s3);
+        } else{
+            if (len1<len2){//ensure len1>len2
+                char temp[2000];
+                strcpy(temp,s1);
+                strcpy(s1,s2);
+                strcpy(s2,temp);
+                int temp2;
+                temp2=len1;
+                len1=len2;
+                len2=temp2;
+            }
+            max_len=len1+1;
+            char s3[max_len+1];
+            int flag=0;
+            int j,k;
+            j=len1-1;
+            k=len2-1;
+            for (int i = max_len-1; i >=1; --i) {
+                if (k>=0){
+                    if (s1[j]+s2[k]+flag-2*'0'>=10){
+                        s3[i]=flag+s1[j]+s2[k]-'0'-10;
+                        flag=1;
+                    } else{
+                        s3[i]=flag+s1[j]+s2[k]-'0';
+                        flag=0;
+                    }
+                    j--;
+                    k--;
+                } else{
+                    if (s1[j]+flag-'0'>=10){
+                        s3[i]=flag+s1[j]-10;
+                        flag=1;
+                    } else{
+                        s3[i]=flag+s1[j];
+                        flag=0;
+                    }
+                    j--;
+                }
+            }
+            if (flag){
+                s3[0]='1';
+                s3[max_len]='\0';
+            } else{
+                max_len=max_len-1;
+                for (int i = 1; i <max_len+1; ++i) {
+                    s3[i-1]=s3[i];
+                }
+                s3[max_len]='\0';
+            }
+            printf("%s\n",s3);
+        }
+    }
     return 0;
 }
---
+++ JudgeScores/cmake-build-debug/CMakeFiles/JudgeScores.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JudgeScores.dir/main.c.obj"
+  "JudgeScores.exe"
+  "JudgeScores.exe.manifest"
+  "JudgeScores.pdb"
+  "libJudgeScores.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/JudgeScores.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
